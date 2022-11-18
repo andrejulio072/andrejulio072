ce187b0011e6c64e8321004c4f0944ac76aff6d0
Andre Garcia <andrejulio072@hotmail.com>
2022-11-18 22:02:38 +0000

    100 days of code

--- prism/module 0/fizzbuzz.js
+++ prism/module 0/fizzbuzz.js
@@ -1,8 +1,27 @@
 function fizzBuzz() {
   let counter=1;
-  while counter <=20:
-  if counter%15=0:
-  
-
+  while (counter <=20){
+    if (counter%15==0){
+console.log("FizzBuzz");
+    }
+    else if (counter%3==0){
+console.log("Fizz");
+    }
+    else if (counter%5==0){
+      console.log("Buzz");
+    }
+    else{
+      console.log(counter);
+    }
+    counter+=1;
+  }
 
 }
+console.log(fizzBuzz());
+
+// for (var i = 1; i < 101; i++) {
+//   if (i % 15 == 0) console.log("FizzBuzz");
+//   else if (i % 3 == 0) console.log("Fizz");
+//   else if (i % 5 == 0) console.log("Buzz");
+//   else console.log(i);
+// }
