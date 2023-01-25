e4a9436fc47cba8aa5a4a9467080d001543dd0e3
Andre Julio Clementino Garcia <85296705+andrejulio072@users.noreply.github.com>
2023-01-25 12:34:30 +0000

    Create code.py

---
+++ hackerRank/week1/LonelyInteger/code.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'lonelyinteger' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+def lonelyinteger(a):
+    # Write your code here
+    if len(a) == 0:
+        return -1
+    ret = []
+    for i in a:
+        if i in ret:
+            ret.remove(i)
+        else:
+            ret.append(i)
+    return ret[0] if len(ret) != 0 else -1
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = lonelyinteger(a)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
